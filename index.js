0f98d01a9847814d9eb3fbbb9788026b4ad53fc1
ValderreyMicaela <115328167+ValderreyMicaela@users.noreply.github.com>
2022-10-29 00:24:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,30 +12,7 @@ navToggle.addEventListener("click", () => {
   }
 });
 
-/*const grid = new Muuri('.grid',{  
-  layout: {
-            rounding: false
-          }
 
-});
-
-
-   const enlaces= document.querySelectorAll('#categorias a');
-   enlaces.forEach( (elemento) => {
-         elemento.addEventListener('click', (evento) => {
-           evento.preventDefault();
-           enlaces.forEach ((enlaces) => enlaces.classList.remove ('activo'));
-           evento.target.classList.add('activo');
-
-           const categoria = evento.target.innerHTML.toLowerCase();
-           categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
-         });
-   });
-
-   document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
-		const busqueda = evento.target.value;
-		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
-	});*/
 
   const formulario = document.getElementById('formulario');
   const inputs = document.querySelectorAll('#formulario input'); 
